50f95b8d5334f9ba94528f8a5bb18d712194a310
rafaelpeinado <rafael.peinado.silva@gmail.com>
2024-07-09 19:00:06 -0300

    wip: desenvolvendo tela de login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export function getAccessToken() {
   const authService = inject(AuthService);
@@ -14,6 +15,7 @@ export function getAccessToken() {
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
     provideRouter(routes),
     provideHttpClient(
       withFetch(),
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,34 +3,22 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { LoginResponse } from '../shared/models/login-response.model';
 import { Router } from '@angular/router';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { AuthService } from '../shared/services/auth.service';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCheckboxModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
-  public currentScreenSize?: string;
-  // Create a map to display breakpoint names for demonstration purposes.
-  private displayNameMap = new Map([
-    [Breakpoints.XSmall, 'XSmall'],
-    [Breakpoints.Small, 'Small'],
-    [Breakpoints.Medium, 'Medium'],
-    [Breakpoints.Large, 'Large'],
-    [Breakpoints.XLarge, 'XLarge'],
-  ]);
-
-
   public formGroup!: FormGroup;
 
   constructor(
@@ -42,21 +30,9 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.breakpointObserver
-      .observe([
-        Breakpoints.XSmall,
-        Breakpoints.Small,
-        Breakpoints.Medium,
-        Breakpoints.Large,
-        Breakpoints.XLarge,
-      ])
-      .pipe()
+      .observe(['(max-width: 600px)'])
       .subscribe(result => {
         console.log('result', result);
-        for (const query of Object.keys(result.breakpoints)) {
-          if (result.breakpoints[query]) {
-            this.currentScreenSize = this.displayNameMap.get(query) ?? 'Unknown';
-          }
-        }
       });
 
     this.formGroup = this.formBuilder.group({
@@ -68,10 +44,7 @@ export class LoginComponent implements OnInit {
   public login(): void {
     this.authService.authenticate(this.formGroup.value)
       .subscribe({
-        next: (loginResponse: LoginResponse) => {
-          console.log('response', loginResponse);
-          this.router.navigate(['']);
-        },
+        complete: () => this.router.navigate(['']),
         error: (error: any) => console.log('error', error),
       });
   }
